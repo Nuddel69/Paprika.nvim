a88377a0f2daf4be75d89394ce05e979b4148b89
Mats Bjønnes <mats.bjonnes@gmail.com>
2026-01-16 20:24:37 +0000

    core: basic options, keymaps and plugin-manager

---
+++ lua/config/options.lua
@@ -0,0 +1,22 @@
+local opt = vim.opt
+
+-- General
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+opt.undofile = true
+opt.undolevels = 10000
+opt.timeoutlen = 300
+opt.virtualedit = "block"
+opt.wrap = false
+
+-- Tab
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftround = true
+opt.shiftwidth = 2
+
+-- UI config
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
